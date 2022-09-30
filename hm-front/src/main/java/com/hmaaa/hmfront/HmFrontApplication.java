913db747187473f82c63b3bc9b4aa9bbf4591242
HMaaa <48823171+HMaaa@users.noreply.github.com>
2022-09-30 16:12:07 +0900

    add: module setting

---
+++ hm-front/src/main/java/com/hmaaa/hmfront/HmFrontApplication.java
@@ -0,0 +1,13 @@
+package com.hmaaa.hmfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmFrontApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HmFrontApplication.class, args);
+  }
+
+}
---
+++ hm-front/src/test/java/com/hmaaa/hmfront/HmFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmaaa.hmfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmFrontApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
